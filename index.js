a22ac90f3b32006026b59944b9eed5414cd80313
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-05 11:26:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+// Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+const copyDrivers = drivers.slice();
+function destructivelyAppendDriver (name){
+  drivers.push(name);
+}
+
+ function destructivelyPrependDriver(name){
+   drivers.unshift(name);
+ }
+ 
+ function destructivelyRemoveLastDriver(){
+   drivers.pop();
+ }
+  function destructivelyRemoveFirstDriver(){
+   drivers.shift();
+ }
+
+
+function appendDriver(name){
+   
+  copyDrivers.push(name);
+}
+
+function prependDriver(name){
+ 
+  copyDrivers.unshift(name);
+}
+
+function removeLastDriver(){
+   copyDrivers.pop();
+}
+function removeFirstDriver(){
+   copyDrivers.shift();
+}
